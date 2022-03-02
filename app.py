5b85b67f1b022858870a2aaca3a13096b3a6ef33
alizarizqi <rizqializa@gmail.com>
2022-03-02 22:37:45 +0700

    changes

--- app.py
+++ app.py
@@ -1,37 +1,56 @@
+
 import telebot
 from flask import Flask, request
 import os
-API_TOKEN = '5159960474:AAH26K6_EjDc-GgeFclKIRbmaPc9hbGTPA4'
 
-bot = telebot.TeleBot(API_TOKEN)
+TOKEN = "5159960474:AAH26K6_EjDc-GgeFclKIRbmaPc9hbGTPA4"
+bot = telebot.TeleBot(token=TOKEN)
 server = Flask(__name__)
-PORT = int(os.environ.get('PORT', '8443'))
-
-# Handle '/start' and '/help'
 
 
-@bot.message_handler(commands=['help', 'start'])
+def findat(msg):
+    # from a list of texts, it finds the one with the '@' sign
+    for i in msg:
+        if '@' in i:
+            return i
+
+
+@bot.message_handler(commands=['start'])  # welcome message handler
 def send_welcome(message):
-    bot.reply_to(
-        message, "\nHi there, I am EchoBot.\nI am here to echo your words. Just send anything and I'll send the same thing to you!\n")
+    bot.reply_to(message, '(placeholder text)')
 
-    # Handle all other messages with content_type 'text' (content_types defaults to ['text'])
 
-    @bot.message_handler(func=lambda message: True)
-    def echo_message(message):
-        bot.reply_to(message, message.text)
+@bot.message_handler(commands=['help'])  # help message handler
+def send_welcome(message):
+    bot.reply_to(message, 'ALPHA = FEATURES MAY NOT WORK')
 
-    @server.route('/' + API_TOKEN, methods=['POST'])
-    def getMessage():
-        bot.process_new_updates(
-            [telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
-        return "!", 200
 
-    @server.route("/")
-    def webhook():
-        bot.remove_webhook()
-        bot.set_webhook(url='https://skripsi2022.herokuapp.com/' + API_TOKEN)
-        return "!", 200
+@bot.message_handler(func=lambda msg: msg.text is not None and '@' in msg.text)
+# lambda function finds messages with the '@' sign in them
+# in case msg.text doesn't exist, the handler doesn't process it
+def at_converter(message):
+    texts = message.text.split()
+    at_text = findat(texts)
+    if at_text == '@':  # in case it's just the '@', skip
+        pass
+    else:
+        insta_link = "https://instagram.com/{}".format(at_text[1:])
+        bot.reply_to(message, insta_link)
 
-    if __name__ == "__main__":
-        server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
+
+@server.route('/' + TOKEN, methods=['POST'])
+def getMessage():
+    bot.process_new_updates(
+        [telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+    return "!", 200
+
+
+@server.route("/")
+def webhook():
+    bot.remove_webhook()
+    bot.set_webhook(url='https://skripsi2022.herokuapp.com/' + TOKEN)
+    return "!", 200
+
+
+if __name__ == "__main__":
+    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))

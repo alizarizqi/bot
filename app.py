bf499570bfde4abe967fe74ed663f1ea487b16d6
alizarizqi <rizqializa@gmail.com>
2022-03-16 14:08:47 +0700

    langdetect

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+from flask import Flask, request
+
 # webhook heroku - this tutorial works on cloud.
 #   to run flask on local server
 #       export FLASK_APP=tutorial6
@@ -7,18 +9,18 @@
 # https://python-telegram-bot.readthedocs.io/
 # https://seminar.io/2018/09/03/building-serverless-telegram-bot/
 # https://www.heroku.com/
-from flask import Flask, render_template, request
 
 import os
 import telegram
-import spacy
-from spacy_langdetect import LanguageDetector
-nlp = spacy.load("en_core_web_sm")
-language_detector = LanguageDetector()
-nlp.add_pipe(language_detector)
+# import spacy
+# from spacy_langdetect import LanguageDetector
+# nlp = spacy.load("en_core_web_sm")
+# language_detector = LanguageDetector()
+# nlp.add_pipe(language_detector)
 
 
 # from langdetect import detect
+from langdetect import detect
 
 app = Flask(__name__)
 
@@ -30,26 +32,30 @@ def webhook():
         update = telegram.Update.de_json(request.get_json(force=True), bot)
         chat_id = update.effective_chat.id
         text = update.message.text
-        # first_name = update.effective_chat.first_name
-        # Reply with the same message
-
-        # kalimat = text.split()
-        # spam2 = len(kalimat)
-        # if(spam2 <= 10):
-        # lang = detect(text)
-        # if(lang == 'en'):
-        # lang = detect(text)
-        doc = nlp(text)
-        lang = doc._.language
+        lang = detect(text)
         bot.send_message(chat_id, lang)
-        # else:
-        #     bot.send_message(
-        #         chat_id, "Sorry, your text is too much. Please write the simple text")
 
-        # if text == '/start':
-        #     bot.send_message(chat_id, "halo")
-        # else:
-        #     bot.sendMessage(chat_id=chat_id, text=f"{text} {first_name}")
+
+# first_name = update.effective_chat.first_name
+# Reply with the same message
+
+# kalimat = text.split()
+# spam2 = len(kalimat)
+# if(spam2 <= 10):
+# lang = detect(text)
+# if(lang == 'en'):
+# lang = detect(text)
+# doc = nlp(text)
+# lang = doc._.language
+# bot.send_message(chat_id, lang)
+# else:
+#     bot.send_message(
+#         chat_id, "Sorry, your text is too much. Please write the simple text")
+
+# if text == '/start':
+#     bot.send_message(chat_id, "halo")
+# else:
+#     bot.sendMessage(chat_id=chat_id, text=f"{text} {first_name}")
 
         return 'ok'
     return 'error'

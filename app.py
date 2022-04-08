4bd10da2a564a2d5509355d0f75c6508937782e4
alizarizqi <rizqializa@gmail.com>
2022-04-08 08:00:49 +0700

    new rule based5

--- app.py
+++ app.py
@@ -23,43 +23,44 @@ def webhook():
         if langgg == 'en':
             checkk = Speller(lang='en')
             sp = checkk(text)
-            textsplit = sp.split()
+            # textsplit = sp.split()
             doc = nlp(sp)
             poss = " ".join(token.pos_ for token in doc)
             possplit = poss.split()
-        grammar = [{
-            "pattern": ["VERB", "PRON"],
-            "pesan":"Kata kerja ditempatkan setelah kata ganti",
-            "koreksi":[1, 0],  # PRON, VERB
-            "contoh":"we read",
-            "status":"error1",
-            "err_id":1}]
+            bot.send_message(chat_id, possplit)
+        # grammar = [{
+        #     "pattern": ["VERB", "PRON"],
+        #     "pesan":"Kata kerja ditempatkan setelah kata ganti",
+        #     "koreksi":[1, 0],  # PRON, VERB
+        #     "contoh":"we read",
+        #     "status":"error1",
+        #     "err_id":1}]
 
-        def grammar_checker(testing, testing_list, testing_pos):
-            check = 0
-            # iterate through possible errors
-            for err in grammar:
-                indexes = []
-                # look for error patterns in the list of POS tags
-                for i in range(len(testing_pos)):
-                    # i += check
-                    # found a match
-                    if testing_pos[i:i+len(err["pattern"])] == err["pattern"]:
-                        indexes.append((i, i+len(err["pattern"])))
-                        if err["err_id"] == 1:
-                            koreksi_pos = [
-                                testing_pos[i:i+len(err["pattern"])][a] for a in err["koreksi"]]
-                            koreksi = [
-                                testing_list[i:i+len(err["pattern"])][a] for a in err["koreksi"]]
-                            for j in range(len(indexes)):
-                                testing_list[i:indexes[j][1]] = koreksi
-                                testing_pos[indexes[j][0]                                            :indexes[j][1]] = koreksi_pos
-                                grammar_id.extend([i, i+1])
+        # def grammar_checker(testing, testing_list, testing_pos):
+        #     check = 0
+        #     # iterate through possible errors
+        #     for err in grammar:
+        #         indexes = []
+        #         # look for error patterns in the list of POS tags
+        #         for i in range(len(testing_pos)):
+        #             # i += check
+        #             # found a match
+        #             if testing_pos[i:i+len(err["pattern"])] == err["pattern"]:
+        #                 indexes.append((i, i+len(err["pattern"])))
+        #                 if err["err_id"] == 1:
+        #                     koreksi_pos = [
+        #                         testing_pos[i:i+len(err["pattern"])][a] for a in err["koreksi"]]
+        #                     koreksi = [
+        #                         testing_list[i:i+len(err["pattern"])][a] for a in err["koreksi"]]
+        #                     for j in range(len(indexes)):
+        #                         testing_list[i:indexes[j][1]] = koreksi
+        #                         testing_pos[indexes[j][0]                                            :indexes[j][1]] = koreksi_pos
+        #                         grammar_id.extend([i, i+1])
 
-        grammar_checker(" ".join(textsplit), textsplit, possplit)
-        output2 = textsplit
-        output3 = ' '.join(output2)
-        bot.sendMessage(chat_id, output3)
+        # grammar_checker(" ".join(textsplit), textsplit, possplit)
+        # output2 = textsplit
+        # output3 = ' '.join(output2)
+        # bot.sendMessage(chat_id, output3)
 
         return 'ok'
     return 'error'
